a2557adc4735d05f92a824ab4bdc539ebe875e61
2814109 <2814109k@gmail.com>
2024-02-05 23:58:39 +0900

    u

--- app/dashboard/components/recent-sales.tsx
+++ app/dashboard/components/recent-sales.tsx
@@ -67,3 +67,28 @@ export function RecentSales() {
     </div>
   );
 }
+
+type BookkeepingType = "Income" | "Expenditure";
+
+type Props = {
+  bookkeepingType: BookkeepingType;
+  itemName: string;
+  cost: number;
+};
+const Row = (props: Props) => {
+  return (
+    <div className="flex items-center">
+      <Avatar className="h-9 w-9">
+        <AvatarImage src="/avatars/01.png" alt="Avatar" />
+        <AvatarFallback>
+          {props.bookkeepingType === "Income" ? "収入" : "支出"}
+        </AvatarFallback>
+      </Avatar>
+      <div className="ml-4 space-y-1">
+        <p className="text-sm font-medium leading-none">{props.itemName}</p>
+        <p className="text-sm text-muted-foreground">雑費</p>
+      </div>
+      <div className="ml-auto font-medium">{props.cost.toLocaleString()}円</div>
+    </div>
+  );
+};
