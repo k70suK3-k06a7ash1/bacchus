e0663e80171b5e1a9011c2c5a663f29d1e6f242a
2814109 <2814109k@gmail.com>
2024-02-08 00:06:19 +0900

    u

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,7 +5,7 @@ import { Label } from "@/components/ui/label";
 export default function Register() {
   return (
     <>
-      <div className="p-4">
+      <div className="py-4 px-8">
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="cost">金額</Label>
           <Input placeholder="金額" id="cost" type="number" />
@@ -14,52 +14,3 @@ export default function Register() {
     </>
   );
 }
-// "use client";
-// import { useState } from "react";
-// import WheelPicker from "react-wheelpicker";
-
-// export default function Register() {
-//   const data = [
-//     "Intro to Data Science",
-//     "Big Data",
-//     "Design and Analysis of Algorithms",
-//     "Operating Systems",
-//     "Cloud Computing",
-//     "Principles of Database Systems",
-//   ];
-
-//   const [isPickerOpen, setIsPickerOpen] = useState(false);
-//   const [selection, setSelection] = useState(data[2]);
-//   //     this.state = {
-//   //       pickerOpen: false,
-//   //       data: ,
-//   //       defaultSelection: 3,
-//   //       selection: "Big Data",
-//   //     };
-//   //   }
-
-//   return (
-//     <>
-//       here
-//       <div className="selected" onClick={() => setIsPickerOpen((pre) => !pre)}>
-//         {selection}
-//       </div>
-//       {isPickerOpen && (
-//         <div className="demo-container">
-//           <WheelPicker
-//             animation="falt"
-//             data={data}
-//             height={40}
-//             parentHeight={250}
-//             fontSize={13}
-//             defaultSelection={3}
-//             updateSelection={(selectedIndex) =>
-//               setSelection(() => data[selectedIndex])
-//             }
-//             scrollerId="scroll-select-subject"
-//           />
-//         </div>
-//       )}
-//     </>
-//   );
-// }
