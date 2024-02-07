600742aeb43b89468fe16f6f888bf129ea9c7b06
2814109 <2814109k@gmail.com>
2024-02-08 00:00:38 +0900

    u

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,16 @@
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Label } from "@/components/ui/label";
+
 export default function Register() {
   return (
     <>
-      <h1>hey</h1>
+      <div className="p-4">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="cost">金額</Label>
+          <Input placeholder="金額" id="cost" type="number" />
+        </div>
+      </div>
     </>
   );
 }
