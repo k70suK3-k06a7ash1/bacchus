c31131d54e6729580b17147f34fb841404eec017
2814109 <2814109k@gmail.com>
2024-02-14 20:47:19 +0900

    u

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-import Image from "next/image";
 
 import { Button } from "@/registry/new-york/ui/button";
 import {
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -6,8 +6,17 @@ export default function Register() {
     <>
       <div className="py-4 px-8">
         <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="cost">金額</Label>
+          <Label htmlFor="cost">品目</Label>
           <Input placeholder="金額" id="cost" type="number" />
+        </div>
+
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="cost">金額</Label>
+          <Input placeholder="現地通貨" id="cost" type="number" />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="cost">金額</Label>
+          <Input disabled placeholder="母国通貨" id="cost" type="number" />
         </div>
       </div>
     </>
