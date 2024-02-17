163960b74363d84eecf82382af402f7b007b73ea
2814109 <2814109k@gmail.com>
2024-02-17 20:14:34 +0900

    u

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 
-// const inter = Inter({ subsets: ["latin"] });
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Spacer } from "@nextui-org/spacer";
+import { useState } from "react";
 
 export default function Register() {
+  const [cost, setCost] = useState(0);
   return (
     <>
       <div className="py-4 px-8">
@@ -13,13 +16,24 @@ export default function Register() {
         <Spacer y={4} />
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="cost">金額</Label>
-          <Input placeholder="現地通貨" id="cost" type="number" />
+          <Input
+            onChange={(value) => setCost(Number(value.currentTarget.value))}
+            placeholder="現地通貨"
+            id="cost"
+            type="number"
+          />
         </div>
         <Spacer y={4} />
 
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="cost">金額</Label>
-          <Input disabled placeholder="母国通貨" id="cost" type="number" />
+          <Input
+            value={cost}
+            disabled
+            placeholder="母国通貨"
+            id="cost"
+            type="number"
+          />
         </div>
       </div>
     </>
