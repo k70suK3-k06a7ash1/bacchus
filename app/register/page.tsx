53f12128278442cf9249b709f7992af33264ccd0
2814109 <2814109k@gmail.com>
2024-02-19 01:18:53 +0900

    u

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Spacer } from "@nextui-org/spacer";
 import { ReactNode, useState } from "react";
-
+import { CgAddR } from "react-icons/cg";
 type TComposition = {
   children: ReactNode;
 };
@@ -14,19 +14,25 @@ export default function Register() {
       <div className="py-4 px-8">
         <InputWrapper>
           <Label htmlFor="cost">品目</Label>
-          <Input placeholder="品目" id="cost" type="number" />
+          <Input size={18} placeholder="品目" id="cost" type="number" />
         </InputWrapper>
         <Spacer y={4} />
         <InputWrapper>
           <Label htmlFor="cost">金額</Label>
           <Center currency="円">
             <Input
+              size={18}
               onChange={(value) => setCost(Number(value.currentTarget.value))}
               id="cost"
               type="number"
             />
           </Center>
         </InputWrapper>
+        <Spacer y={8} />
+        <div className="flex justify-center">
+          <CgAddR size={48} />
+        </div>
+
         {/* <Spacer y={4} /> */}
         {/* <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="cost">金額</Label>
