1b9179898b8d55df58332cda7c517123fd8aca1d
2814109 <2814109k@gmail.com>
2024-02-18 17:02:01 +0900

    u

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,8 +2,11 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Spacer } from "@nextui-org/spacer";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 
+type TComposition = {
+  children: ReactNode;
+};
 export default function Register() {
   const [cost, setCost] = useState(0);
   return (
@@ -16,26 +19,39 @@ export default function Register() {
         <Spacer y={4} />
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="cost">金額</Label>
-          <Input
-            onChange={(value) => setCost(Number(value.currentTarget.value))}
-            placeholder="現地通貨"
-            id="cost"
-            type="number"
-          />
+          <Center currency="円">
+            <Input
+              onChange={(value) => setCost(Number(value.currentTarget.value))}
+              placeholder="現地通貨"
+              id="cost"
+              type="number"
+            />
+          </Center>
         </div>
         <Spacer y={4} />
-
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="cost">金額</Label>
-          <Input
-            value={cost}
-            disabled
-            placeholder="母国通貨"
-            id="cost"
-            type="number"
-          />
+          <Center currency="円">
+            <Input
+              value={cost}
+              disabled
+              placeholder="母国通貨"
+              id="cost"
+              type="number"
+            />
+          </Center>
         </div>
       </div>
     </>
   );
 }
+
+type Props = {
+  currency: string;
+};
+const Center = ({ children, currency }: TComposition & Props) => (
+  <div className="flex items-center">
+    {children} <Spacer x={2} />
+    {currency}
+  </div>
+);
